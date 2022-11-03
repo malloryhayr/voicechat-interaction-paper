847188beb816225098dcb0b263de0652aae773f2
iGalaxyYT <contact@igalaxy.dev>
2022-11-02 19:25:53 -0700

    fix: player name in cmd output

--- src/main/java/dev/igalaxy/voicechatinteraction/VoiceChatInteractionCommand.java
+++ src/main/java/dev/igalaxy/voicechatinteraction/VoiceChatInteractionCommand.java
@@ -23,8 +23,8 @@ public class VoiceChatInteractionCommand implements CommandExecutor {
                     if (args.length >= 2 && player.hasPermission("voicechat_interaction.toggle.others")) {
                         Player other = Bukkit.getPlayer(args[1]);
                         plugin.setInteractionToggle(other, !plugin.getInteractionToggle(other));
-                        player.sendMessage("Interactions toggled to " + plugin.getInteractionToggle(other) + " for " + other.name());
-                        other.sendMessage(player.name() + " toggled your interactions to " + plugin.getInteractionToggle(other));
+                        player.sendMessage("Interactions toggled to " + plugin.getInteractionToggle(other) + " for " + other.getName());
+                        other.sendMessage(player.getName() + " toggled your interactions to " + plugin.getInteractionToggle(other));
                     } else if (player.hasPermission("voicechat_interaction.toggle")) {
                         plugin.setInteractionToggle(player, !plugin.getInteractionToggle(player));
                         player.sendMessage("Interactions toggled to " + plugin.getInteractionToggle(player) + " for yourself");
